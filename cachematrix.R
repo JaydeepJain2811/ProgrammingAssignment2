f0473459a5be01e81d755a214af9d78b976f47a7
JaydeepJain2811 <65348056+JaydeepJain2811@users.noreply.github.com>
2020-05-15 06:21:15 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,34 +4,57 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
+  ## @x: a square invertible matrix
+  ## return: a list containing functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list is used as the input to cacheSolve()
+  
+  inv = NULL
+  set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
     x <<- y
-    m <<- NULL
+    inv <<- NULL
   }
-  get <- function() x
-  setinverse <- function(solveMatrix) m <<- solveMatrix
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-
-
-
-## Write a short comment describing this function
-
-
-cachesolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
+cacheSolve <- function(x, ...) {
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
+  
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    # get it from the cache and skips the computation. 
     message("getting cached data")
-    return(m)
+    return(inv)
   }
-  data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
-  m
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  
+  return(inv)
 }
+
+
+
+#testing the formula
+B <- matrix(c(1,2,3,4),2,2)
+#solve(B) #We pretend that this cant't happen xD
+
+B1 <- makeCacheMatrix(B)
+B1
+cacheSolve(B1) #inverse returned after computation
+cacheSolve(B1) #inverse returned from cache
